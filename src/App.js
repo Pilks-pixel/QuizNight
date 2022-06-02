f150a8c66c516a62d3851c4d9bd1488f6c568af4
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-06-02 12:44:15 +0100

    fetch questions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import Questions from './components/Questions';
 import AnswerButtons from './components/AnswerButtons';
-import { useEffect, useState, useRef } from 'react';
-import quizData from "./quizData";
+import { useState, useRef, useEffect } from 'react';
+
+
 
 
 function App() {
 
   const [showQuestion, setShowQuestion] = useState(false)
   const [score, setScore] = useState(0)
-  const [questionNum, setQuestionNum] = useState(1)
-  const [quizQuestion, setQuizQuestion] = useState(quizData.results[0])
+  const [quizData, setQuizData] = useState([])
+  const [questionNum, setQuestionNum] = useState(0)
   const [gameFinished, setGameFinished] = useState(false)
   const [buttonStyles, setButtonStyles] = useState({backgroundColor: '#f5f5f5'}) 
   const btnDisable = useRef();
+
+  console.log(quizData)
+  console.log(quizData[questionNum])
+
 
   
 
@@ -21,26 +26,36 @@ function App() {
     return setShowQuestion(prevShowQuestion => !prevShowQuestion)
   }
 
-  function answerClick(a) {
-    if (questionNum === quizData.results.length) {
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10')
+    .then(response => response.json())
+    .then(data => setQuizData(data.results));
+  },[])
+
+  
+    function answerClick(a) {
+
+    if (questionNum === quizData.length - 1) {
         setGameFinished(true)
-        btnDisable.current.disabled = true
-        a === quizQuestion.correct_answer? setButtonStyles({backgroundColor: '#00FF00'}) : setButtonStyles({backgroundColor: 'red'})
-        return a === quizQuestion.correct_answer? setScore(prevScore => prevScore + 1) :
+        // btnDisable.current.disabled = true
+        a === quizData.correct_answer? setButtonStyles({backgroundColor: '#00FF00'}) : setButtonStyles({backgroundColor: 'red'})
+        return a === quizData[questionNum].correct_answer? setScore(prevScore => prevScore + 1) :
         score
     } else {
-        btnDisable.current.disabled = true
-        a === quizQuestion.correct_answer? setButtonStyles({backgroundColor: '#00FF00'}) : setButtonStyles({backgroundColor: 'red'})
-        setTimeout(() => {setQuizQuestion(quizData.results[questionNum])}, 400)
+        // btnDisable.current.disabled = true
+        a === quizData[questionNum].correct_answer? setButtonStyles({backgroundColor: '#00FF00'}) : setButtonStyles({backgroundColor: 'red'})
+        // setTimeout(() => {setQuizQuestion(quizData[questionNum])}, 400)
         setTimeout(() => {setQuestionNum(prevQuestionNum => prevQuestionNum + 1)}, 400)
         setTimeout(() => {setButtonStyles({backgroundColor: '#f5f5f5'})}, 400)
-        setTimeout(() => {btnDisable.current.disabled = false}, 400)
-        return a === quizQuestion.correct_answer? setScore(prevScore => prevScore + 1) :
+        // setTimeout(() => {btnDisable.current.disabled = false}, 400)
+        return a === quizData[questionNum].correct_answer? setScore(prevScore => prevScore + 1) :
         score
         
     }
-}
+  } 
 
+  
+  
 
   return (
     <div className="App">
@@ -50,15 +65,14 @@ function App() {
 
       { showQuestion && 
       <>
-      <Questions question={quizQuestion}/>
-      <AnswerButtons 
-      question={quizQuestion} 
-      qNumber={questionNum} 
-      newQuestion={answerClick} 
-      styles={buttonStyles} 
-      btnOff={btnDisable}
-
-      />
+        <Questions question={quizData[questionNum]}/>
+        <AnswerButtons 
+        question={quizData[questionNum]} 
+        qNumber={questionNum} 
+        newQuestion={answerClick} 
+        styles={buttonStyles} 
+        btnOff={btnDisable}
+        />
       </>
       }
 
--- src/components/AnswerButtons.js
+++ src/components/AnswerButtons.js
@@ -1,21 +1,29 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState} from "react";
 
 export default function AnswerButtons(props) {
 
     // Creates an array of all answers and places them in random order before rendering each as a button
-    const answers = [...props.question.incorrect_answers, props.question.correct_answer]
+    
 
-    useEffect(() => {
-        answers.sort(() => Math.random() - 0.5)
-        console.log(answers)
-    },[props.question])
-    
-    
+        
+        function randomAnswers() {
+            const answers = [...props.question.incorrect_answers, props.question.correct_answer]
+            return answers.sort(() => Math.random() - 0.5)
+            
+        }
+        console.log(randomAnswers())
+        
+        useEffect(() => {
+            randomAnswers();
+            
+        },[props.newQuestion])
+        
+        const answerBtnElements = randomAnswers().map((a) => <button style={props.styles}  ref={props.btnOff} onClick={() => props.newQuestion(a)}>{a}</button>)
         
         
     return (
             <>
-            {answers.map((a) => <button style={props.styles}  ref={props.btnOff} onClick={() => props.newQuestion(a, props.btnOff)}>{a}</button>)}
+            {answerBtnElements}
             </>
         )
 }
